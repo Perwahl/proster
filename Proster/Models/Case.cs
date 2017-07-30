d5f3e8ad740d872f38c6940097e43608568499ed
Rebecka <rebeckamerkel@gmail.com>
2017-07-30 13:00:42 +0200

    add rca classes

---
+++ Proster/Models/Case.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proster.Models
+{
+    public class Case
+    {
+        public int CaseId { get; set; }
+        public string CaseUrl { get; set; }
+    }
+}
---
+++ Proster/Models/RCA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proster.Models
+{
+    public class RCA
+    {
+        public int RcaId { get; set; }
+        public Case Case { get; set; }
+        public User User { get; set; }
+        public string Component { get; set; }
+        public string RootCause { get; set; }
+        public string RootCauseDescripton { get; set; }
+        public string CorrectiveAction { get; set; }
+    }
+}
